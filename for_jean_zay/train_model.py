9b0ccc2f3fa2e157d112e348d8f96d3979144aad
RaphaelNotarantonio <57987148+RaphaelNotarantonio@users.noreply.github.com>
2020-09-18 18:35:25 +0200

    Update train_model.py

--- for_jean_zay/train_model.py
+++ for_jean_zay/train_model.py
@@ -127,7 +127,7 @@ def main():
   # Load BertForSequenceClassification, the pretrained BERT model with a single 
   # linear classification layer on top. 
   modd = RobertaForSequenceClassification.from_pretrained(
-    "roberta-base", # Use the 12-layer BERT model, with an uncased vocab.
+    "./my_pretrained", # Use the 12-layer BERT model, with an uncased vocab.
     num_labels = 2, # The number of output labels--2 for binary classification.
                     # You can increase this for multi-class tasks.   
     output_attentions = False, # Whether the model returns attentions weights.
