d235cbe75788bbc9261ed36c2a5df1d10422f89d
RaphaelNotarantonio <57987148+RaphaelNotarantonio@users.noreply.github.com>
2020-09-18 14:30:52 +0200

    Update train_model.py

--- for_jean_zay/train_model.py
+++ for_jean_zay/train_model.py
@@ -14,6 +14,8 @@ import numpy as np
 import time
 import datetime
 import random
+import wget
+import os
 
 # see https://mccormickml.com/2019/07/22/BERT-fine-tuning/
 
@@ -37,7 +39,168 @@ def format_time(elapsed):
 
 
 def main():
+    
+  #first model/finetuning/dataset: language model
+  print('Downloading dataset...')
 
+  # The URL for the dataset zip file.
+  url = 'https://nyu-mll.github.io/CoLA/cola_public_1.1.zip'
+
+  # Download the file (if we haven't already)
+  if not os.path.exists('./cola_public_1.1.zip'):
+        wget.download(url, './cola_public_1.1.zip')
+        
+  # Unzip the dataset (if we haven't already)
+  if not os.path.exists('./cola_public/'):
+    !unzip cola_public_1.1.zip
+    
+ 
+  sentences2=[]
+  labels2=[]
+  with open("./cola_public/raw/in_domain_train.tsv") as tsvfile2:
+        tsvreader2 = csv.reader(tsvfile2, delimiter="\t")
+        for line in tsvreader2:
+            sentences2 += [line[3]]
+            labels2 += [int(line[1])]
+            
+  #load encoder
+  tokenizer = RobertaTokenizer.from_pretrained('roberta-base') 
+    
+  # ici juste un tour pour voir quelle est la taille max . on visera un peu pls haut par sécurité.
+
+  max_len2 = 0
+
+  # For every sentence...
+  for sent in sentences2:
+
+    # Tokenize the text and add `[CLS]` and `[SEP]` tokens.
+    input_ids2 = tokenizer.encode(sent, add_special_tokens=True)
+
+    # Update the maximum sentence length.
+    max_len2 = max(max_len2, len(input_ids))
+
+  print('Max sentence length: ', max_len2)
+
+  input_ids2 = []
+
+  for sent in sentences2:
+    encoded_dict2 = tokenizer.encode(
+                        sent,                      # Sentence to encode.
+                        add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+                        max_length = 64,           # Pad & truncate all sentences.  "max_len2?
+                        pad_to_max_length = True,
+                        return_tensors = 'pt',     # Return pytorch tensors.
+                   )
+  # Add the encoded sentence to the list.    
+  input_ids2.append(encoded_dict2)
+    
+
+  # Convert the lists into tensors.
+  input_ids2 = torch.cat(input_ids2, dim=0)
+  labels2 = torch.tensor(labels2)
+
+  # Print sentence 0, now as a list of IDs.
+  print('Original: ', sentences2[0])
+  print('Token IDs:', input_ids2[0])
+    
+  # Combine the training inputs into a TensorDataset.
+  dataset2 = TensorDataset(input_ids2, labels2)
+
+  # Create a 90-10 train-validation split.
+
+  # Calculate the number of samples to include in each set.
+  train_size2 = int(0.9 * len(dataset2))
+  val_size2 = len(dataset2) - train_size2
+
+  # Divide the dataset by randomly selecting samples.
+  train_dataset2, val_dataset2 = random_split(dataset2, [train_size2, val_size2])
+
+ 
+
+  # The DataLoader needs to know our batch size for training, so we specify it 
+  # here. For fine-tuning BERT on a specific task, the authors recommend a batch 
+  #size of 16 or 32.
+  batch_size = 32
+
+  # Create the DataLoaders for our training and validation sets. 
+  # We'll take training samples in random order. 
+  train_dataloader2 = DataLoader(
+            train_dataset2,  # The training samples.
+            sampler = RandomSampler(train_dataset2), # Select batches randomly
+            batch_size = batch_size # Trains with this batch size.
+          )
+
+  # For validation the order doesn't matter, so we'll just read them sequentially.
+  validation_dataloader2 = DataLoader(
+            val_dataset2, # The validation samples.
+            sampler = SequentialSampler(val_dataset2), # Pull out batches sequentially.
+            batch_size = batch_size # Evaluate with this batch size.
+          )
+    
+
+  # Load BertForSequenceClassification, the pretrained BERT model with a single 
+  # linear classification layer on top. 
+  modd = RobertaForSequenceClassification.from_pretrained(
+    "roberta-base", # Use the 12-layer BERT model, with an uncased vocab.
+    num_labels = 2, # The number of output labels--2 for binary classification.
+                    # You can increase this for multi-class tasks.   
+    output_attentions = False, # Whether the model returns attentions weights.
+    output_hidden_states = False, # Whether the model returns all hidden-states.
+  ) 
+  # If there's a GPU available...
+  if torch.cuda.is_available():  
+      # Tell pytorch to run this model on the GPU.
+      modd.cuda()
+
+  # see https://mccormickml.com/2019/07/22/BERT-fine-tuning/
+
+  from transformers import AdamW
+
+  # Note: AdamW is a class from the huggingface library (as opposed to pytorch) 
+  # I believe the 'W' stands for 'Weight Decay fix"
+  optimizer2 = AdamW(modd.parameters(),
+                  lr = 2e-5, # args.learning_rate - default is 5e-5, our notebook had 2e-5
+                  eps = 1e-8 # args.adam_epsilon  - default is 1e-8.
+                )
+
+  from transformers import get_linear_schedule_with_warmup
+
+  # Number of training epochs. The BERT authors recommend between 2 and 4. 
+  # We chose to run for 4, but we'll see later that this may be over-fitting the
+  # training data.
+  epochs = 4
+
+  # Total number of training steps is [number of batches] x [number of epochs]. 
+  # (Note that this is not the same as the number of training samples).
+  total_steps = len(train_dataloader2) * epochs
+
+  # Create the learning rate scheduler.
+  scheduler = get_linear_schedule_with_warmup(optimizer2, 
+                                            num_warmup_steps = 0, # Default value in run_glue.py
+                                            num_training_steps = total_steps)
+
+  # If there's a GPU available...
+  if torch.cuda.is_available():    
+
+      # Tell PyTorch to use the GPU.    
+      device = torch.device("cuda")
+
+      print('There are %d GPU(s) available.' % torch.cuda.device_count())
+
+      print('We will use the GPU:', torch.cuda.get_device_name(0))
+
+  # If not...
+  else:
+      print('No GPU available, using the CPU instead.')
+      device = torch.device("cpu")
+
+  
+    
+    
+   
+  #second dataset
+    
+  
   #extract data
   sentences=[]
   labels=[]
@@ -48,9 +211,6 @@ def main():
           sentences += [line[0]]
           labels += [int(line[1])]
 
-
-  #load encoder
-  tokenizer = RobertaTokenizer.from_pretrained('roberta-base') 
 
 
   #what is sentence maximum lenght?
@@ -166,21 +326,220 @@ def main():
 
 
 
-  
-  # If there's a GPU available...
-  if torch.cuda.is_available():    
+  #the two trainings
 
-      # Tell PyTorch to use the GPU.    
-      device = torch.device("cuda")
+  import random
+  import numpy as np
 
-      print('There are %d GPU(s) available.' % torch.cuda.device_count())
+  # This training code is based on the `run_glue.py` script here:
+  # https://github.com/huggingface/transformers/blob/5bfcd0485ece086ebcbed2d008813037968a9e58/examples/run_glue.py#L128
 
-      print('We will use the GPU:', torch.cuda.get_device_name(0))
+  # Set the seed value all over the place to make this reproducible.
+  seed_val = 42
 
-  # If not...
-  else:
-      print('No GPU available, using the CPU instead.')
-      device = torch.device("cpu")
+  random.seed(seed_val)
+  np.random.seed(seed_val)
+  torch.manual_seed(seed_val)
+  torch.cuda.manual_seed_all(seed_val)
+
+  # We'll store a number of quantities such as training and validation loss, 
+  # validation accuracy, and timings.
+  training_stats = []
+
+  # Measure the total training time for the whole run.
+  total_t0 = time.time()
+
+  # For each epoch...
+  for epoch_i in range(0, epochs):
+    
+    # ========================================
+    #               Training
+    # ========================================
+    
+    # Perform one full pass over the training set.
+
+    print("")
+    print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
+    print('Training...')
+
+    # Measure how long the training epoch takes.
+    t0 = time.time()
+
+    # Reset the total loss for this epoch.
+    total_train_loss = 0
+
+    # Put the model into training mode. Don't be mislead--the call to 
+    # `train` just changes the *mode*, it doesn't *perform* the training.
+    # `dropout` and `batchnorm` layers behave differently during training
+    # vs. test (source: https://stackoverflow.com/questions/51433378/what-does-model-train-do-in-pytorch)
+    modd.train()
+
+    # For each batch of training data...
+    for step, batch in enumerate(train_dataloader2):
+
+        # Progress update every 40 batches.
+        if step % 40 == 0 and not step == 0:
+            # Calculate elapsed time in minutes.
+            elapsed = format_time(time.time() - t0)
+            
+            # Report progress.
+            print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(train_dataloader2), elapsed))
+
+        # Unpack this training batch from our dataloader. 
+        #
+        # As we unpack the batch, we'll also copy each tensor to the GPU using the 
+        # `to` method.
+        #
+        # `batch` contains three pytorch tensors:
+        #   [0]: input ids 
+        #   [1]: attention masks
+        #   [2]: labels 
+        b_input_ids = batch[0].to(device)
+        b_labels = batch[1].to(device)
+
+        # Always clear any previously calculated gradients before performing a
+        # backward pass. PyTorch doesn't do this automatically because 
+        # accumulating the gradients is "convenient while training RNNs". 
+        # (source: https://stackoverflow.com/questions/48001598/why-do-we-need-to-call-zero-grad-in-pytorch)
+        modd.zero_grad()        
+
+        # Perform a forward pass (evaluate the model on this training batch).
+        # The documentation for this `model` function is here: 
+        # https://huggingface.co/transformers/v2.2.0/model_doc/bert.html#transformers.BertForSequenceClassification
+        # It returns different numbers of parameters depending on what arguments
+        # arge given and what flags are set. For our useage here, it returns
+        # the loss (because we provided labels) and the "logits"--the model
+        # outputs prior to activation.
+        loss, logits = modd(b_input_ids, 
+                             token_type_ids=None, 
+                             labels=b_labels)
+
+        # Accumulate the training loss over all of the batches so that we can
+        # calculate the average loss at the end. `loss` is a Tensor containing a
+        # single value; the `.item()` function just returns the Python value 
+        # from the tensor.
+        total_train_loss += loss.item()
+
+        # Perform a backward pass to calculate the gradients.
+        loss.backward()
+
+        # Clip the norm of the gradients to 1.0.
+        # This is to help prevent the "exploding gradients" problem.
+        torch.nn.utils.clip_grad_norm_(modd.parameters(), 1.0)
+
+        # Update parameters and take a step using the computed gradient.
+        # The optimizer dictates the "update rule"--how the parameters are
+        # modified based on their gradients, the learning rate, etc.
+        optimizer2.step()
+
+        # Update the learning rate.
+        scheduler2.step()
+
+    # Calculate the average loss over all of the batches.
+    avg_train_loss = total_train_loss / len(train_dataloader2)            
+    
+    # Measure how long this epoch took.
+    training_time = format_time(time.time() - t0)
+
+    print("")
+    print("  Average training loss: {0:.2f}".format(avg_train_loss))
+    print("  Training epcoh took: {:}".format(training_time))
+        
+    # ========================================
+    #               Validation
+    # ========================================
+    # After the completion of each training epoch, measure our performance on
+    # our validation set.
+
+    print("")
+    print("Running Validation...")
+
+    t0 = time.time()
+
+    # Put the model in evaluation mode--the dropout layers behave differently
+    # during evaluation.
+    modd.eval()
+
+    # Tracking variables 
+    total_eval_accuracy = 0
+    total_eval_loss = 0
+    nb_eval_steps = 0
+
+    # Evaluate data for one epoch
+    for batch in validation_dataloader2:
+        
+        # Unpack this training batch from our dataloader. 
+        #
+        # As we unpack the batch, we'll also copy each tensor to the GPU using 
+        # the `to` method.
+        #
+        # `batch` contains three pytorch tensors:
+        #   [0]: input ids 
+        #   [1]: attention masks
+        #   [2]: labels 
+        b_input_ids = batch[0].to(device)
+        b_labels = batch[1].to(device)
+        
+        # Tell pytorch not to bother with constructing the compute graph during
+        # the forward pass, since this is only needed for backprop (training).
+        with torch.no_grad():        
+
+            # Forward pass, calculate logit predictions.
+            # token_type_ids is the same as the "segment ids", which 
+            # differentiates sentence 1 and 2 in 2-sentence tasks.
+            # The documentation for this `model` function is here: 
+            # https://huggingface.co/transformers/v2.2.0/model_doc/bert.html#transformers.BertForSequenceClassification
+            # Get the "logits" output by the model. The "logits" are the output
+            # values prior to applying an activation function like the softmax.
+            (loss, logits) = modd(b_input_ids, 
+                                   token_type_ids=None, 
+                                   labels=b_labels)
+            
+        # Accumulate the validation loss.
+        total_eval_loss += loss.item()
+
+        # Move logits and labels to CPU
+        logits = logits.detach().cpu().numpy()
+        label_ids = b_labels.to('cpu').numpy()
+
+        # Calculate the accuracy for this batch of test sentences, and
+        # accumulate it over all batches.
+        total_eval_accuracy += flat_accuracy(logits, label_ids)
+        
+
+    # Report the final accuracy for this validation run.
+    avg_val_accuracy = total_eval_accuracy / len(validation_dataloader2)
+    print("  Accuracy: {0:.2f}".format(avg_val_accuracy))
+
+    # Calculate the average loss over all of the batches.
+    avg_val_loss = total_eval_loss / len(validation_dataloader2)
+    
+    # Measure how long the validation run took.
+    validation_time = format_time(time.time() - t0)
+    
+    print("  Validation Loss: {0:.2f}".format(avg_val_loss))
+    print("  Validation took: {:}".format(validation_time))
+
+    # Record all statistics from this epoch.
+    training_stats.append(
+        {
+            'epoch': epoch_i + 1,
+            'Training Loss': avg_train_loss,
+            'Valid. Loss': avg_val_loss,
+            'Valid. Accur.': avg_val_accuracy,
+            'Training Time': training_time,
+            'Validation Time': validation_time
+        }
+    )
+
+  print("") 
+  print("Training complete!")
+
+  print("Total training took {:} (h:mm:ss)".format(format_time(time.time()-total_t0)))
+
+  #save model
+  torch.save(modd.state_dict(), "languagemodel_finetuned.pt")
+
 
 
 
