b80bd2da2544fc5e60ac7e124db9c82b33c2e34c
RaphaelNotarantonio <57987148+RaphaelNotarantonio@users.noreply.github.com>
2020-10-21 17:07:36 +0200

    Update attack_all.py

--- for_jean_zay/attack_all.py
+++ for_jean_zay/attack_all.py
@@ -30,6 +30,32 @@ from advertorch.attacks.utils import rand_init_delta
 
 #required functions:
 
+def levenshtein(seq1, seq2): #from https://stackabuse.com/levenshtein-distance-and-text-similarity-in-python/
+    size_x = len(seq1) + 1
+    size_y = len(seq2) + 1
+    matrix = np.zeros ((size_x, size_y))
+    for x in range(size_x):
+        matrix [x, 0] = x
+    for y in range(size_y):
+        matrix [0, y] = y
+
+    for x in range(1, size_x):
+        for y in range(1, size_y):
+            if seq1[x-1] == seq2[y-1]:
+                matrix [x,y] = min(
+                    matrix[x-1, y] + 1,
+                    matrix[x-1, y-1],
+                    matrix[x, y-1] + 1
+                )
+            else:
+                matrix [x,y] = min(
+                    matrix[x-1,y] + 1,
+                    matrix[x-1,y-1] + 1,
+                    matrix[x,y-1] + 1
+                )
+    #print (matrix)
+    return (matrix[size_x - 1, size_y - 1])
+
 def sentlong(x): #real length of an input_id sentence 
   compteur = 0
   while compteur<len(x) and x[compteur]!=2:
